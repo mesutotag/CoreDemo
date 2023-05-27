4d4fec8804a5e79c0edfb533c7443825dc0f0d28
mesutotag.mo@gmail.com <mesutotag@123>
2023-05-27 17:56:20 +0300

    ,

---
+++ DataAccessLayer/Abstract/IBlogDal.cs
@@ -0,0 +1,21 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Abstract
+{
+    public interface IBlogDal
+    {
+
+        List<Blog> ListAllBlog();
+        void AddCategory(Blog category);
+        void DeleteCategory(Blog category);
+        void UpdateCategory(Blog category);
+
+        Blog GetByİd(int id);
+
+    }
+}
