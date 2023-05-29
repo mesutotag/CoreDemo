15960057784a81584fc10384d8e4c87ffe6ab336
mesutotag.mo@gmail.com <mesutotag@123>
2023-05-29 20:16:27 +0300

    .

--- DataAccessLayer/Repositories/CommentRepository.cs
+++ DataAccessLayer/Repositories/CommentRepository.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,31 @@ using System.Threading.Tasks;
 
 namespace DataAccessLayer.Repositories
 {
-    internal class CommentRepository
+    public class CommentRepository : IGenericDal<Comment>
     {
+        public void Delete(Comment t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Comment GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Comment> GetListAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(Comment t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Comment t)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
